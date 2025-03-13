d23190f0619253854fc8f25d9073449527461829
cherif <cherif44@outlook.fr>
2025-03-13 12:00:17 +0100

    db

--- users/email_utils.py
+++ users/email_utils.py
@@ -1,7 +1,6 @@
 from django.utils import timezone
 from datetime import timedelta
 from django.core.mail import EmailMessage
-from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.sites.shortcuts import get_current_site
